393d91fa48491e835dd03cb4978d96217eba22ff
joan <janglada@gmail.com>
2021-12-23 11:02:35 +0100

    day 28 **

--- days/day8/src/main.rs
+++ days/day8/src/main.rs
@@ -1,6 +1,5 @@
 extern crate core;
 
-
 use day8::part1::solve_part1;
 use day8::part2::solve_part2;
 
@@ -8,7 +7,7 @@ use std::io::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<Error>> {
-    solve_part1().await;
+    // solve_part1().await;
     solve_part2().await;
 
     Ok(())
--- days/day8/src/part2.rs
+++ days/day8/src/part2.rs
@@ -2,12 +2,116 @@ extern crate core;
 
 use core::utils::donwload_puzzle;
 
-// use crate::part2::Digit::One;
+// use crate::part2::Digit::one;
 use crate::part2::Digit::*;
+use itertools::Itertools;
 
 pub async fn solve_part2() {
-    let _input: String = donwload_puzzle(8).await.unwrap();
+    let _input_str: String = donwload_puzzle(8).await.unwrap();
+    let inputs: Vec<Input> = parse(_input_str.to_string());
+    let r = inputs
+        .iter()
+        .map(|_input| {
+            // println!("-------------------------------------------------------");
+            // println!("data {:?},  digits {:?}", _input.data, _input.digits);
+            let four = _input.find_by_digit(Digit::Four);
+            let seven = _input.find_by_digit(Digit::Seven);
+            let one = _input.find_by_digit(Digit::One);
+            let eight = _input.find_by_digit(Digit::Eight);
+
+            // all of len =  6
+            // (0 & 6 & 9 ) | 4 = nine
+            let six = *_input
+                .find_by_len(6)
+                .iter()
+                .find(|&&n| (n.v | one.v) == eight.v)
+                // .map(|n| n.v)
+                .unwrap();
+
+            // 5 | 6 = 8
+            let five = *_input
+                .find_by_len(5)
+                .iter()
+                .find(|&&n| (n.v | six.v) == six.v)
+                // .map(|n| n.v)
+                .unwrap();
+
+            // 2 | 4 = 8
+            let two = *_input
+                .find_by_len(5)
+                .iter()
+                .filter(|&&n| n.v != five.v)
+                .find(|&&n| (n.v | four.v) == eight.v)
+                // .map(|n| n.v)
+                .unwrap();
+
+            let three = *_input
+                .find_by_len(5)
+                .iter()
+                .find(|&&n| n.v != five.v && n.v != two.v)
+                .unwrap();
+            // 0 xor 8 = 2 & 3 & 4 & 5
+            let zero = *_input
+                .find_by_len(6)
+                .iter()
+                .filter(|&&n| n.v != six.v)
+                .find(|&&n| n.v ^ eight.v == two.v & three.v & four.v & five.v)
+                .unwrap();
+
+            let nine = *_input
+                .find_by_len(6)
+                .iter()
+                .find(|&&n| n.v != six.v && n.v != zero.v)
+                .unwrap();
+
+            // println!("five {}", five);
+            // println!("six {}", (*six).v);
+            // println!("five {}", (*five).v);
+            // println!("two {}", (*two).v);
+            // println!("zero {}", (*zero).v);
+            // println!("nine {}", (*nine).v);
+            // println!();
+
+            let row_res = _input
+                .digits
+                .iter()
+                .map(|&v| {
+                    if v == one.v {
+                        '1'
+                    } else if v == two.v {
+                        '2'
+                    } else if v == three.v {
+                        '3'
+                    } else if v == four.v {
+                        '4'
+                    } else if v == five.v {
+                        '5'
+                    } else if v == six.v {
+                        '6'
+                    } else if v == seven.v {
+                        '7'
+                    } else if v == eight.v {
+                        '8'
+                    } else if v == nine.v {
+                        '9'
+                    } else if v == zero.v {
+                        '0'
+                    } else {
+                        panic!("Unknown value")
+                    }
+                })
+                .join("")
+                .parse::<i32>()
+                .unwrap_or(-1);
+            println!("row {}", row_res);
+            row_res
+        })
+        .sum::<i32>();
+    println!("r {}", r);
 }
+
+fn solve(inputs: &mut Input) {}
+#[derive(PartialEq, Debug)]
 enum Digit {
     One,
     Two,
@@ -17,19 +121,33 @@ enum Digit {
     Six,
     Seven,
     Eight,
+    Nine,
 }
-
+#[derive(Debug)]
 struct Number {
     v: u32,
+    len: usize,
     d: Option<Digit>,
 }
 
+struct Data {
+    one: Number,
+    two: Number,
+    three: Number,
+    four: Number,
+    five: Number,
+    six: Number,
+    seven: Number,
+    eight: Number,
+    nine: Number,
+}
+
 impl Number {
-    fn new(v: u32, d: Digit) -> Number {
-        Number { v, d: Some(d) }
+    fn new(v: u32, len: usize, d: Digit) -> Number {
+        Number { v, len, d: Some(d) }
     }
-    fn unknown(v: u32) -> Number {
-        Number { v, d: None }
+    fn unknown(v: u32, len: usize) -> Number {
+        Number { v, len, d: None }
     }
 
     fn set_digit(&mut self, d: Digit) {
@@ -42,13 +160,33 @@ struct Input {
     digits: Vec<u32>,
 }
 
+impl Input {
+    fn find_by_len(&self, len: usize) -> Vec<&Number> {
+        self.data.iter().filter(|n| n.len == len).collect()
+    }
+
+    fn find_by_value(&self, v: u32) -> Vec<&Number> {
+        self.data.iter().filter(|n| n.v == v).collect()
+    }
+
+    fn find_by_digit(&self, digit: Digit) -> &Number {
+        self.data
+            .iter()
+            .find(|n| match &(*n).d {
+                None => false,
+                Some(d) => *d == digit,
+            })
+            .unwrap()
+    }
+}
+
 fn extract_digit(v: u32, len: usize) -> Number {
     match len {
-        2 => Number::new(v, One),   // 1
-        4 => Number::new(v, Four),  // 4
-        3 => Number::new(v, Seven), // 7
-        7 => Number::new(v, Eight), // 8
-        _ => Number::unknown(v),
+        2 => Number::new(v, len, One),   // 1
+        4 => Number::new(v, len, Four),  // 4
+        3 => Number::new(v, len, Seven), // 7
+        7 => Number::new(v, len, Eight), // 8
+        _ => Number::unknown(v, len),
     }
 }
 fn to_int(c: char) -> u32 {
@@ -116,8 +254,7 @@ fn parse(input: String) -> Vec<Input> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
-    
+    use itertools::Itertools;
 
     #[test]
     fn masks1() {
@@ -134,20 +271,6 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 
         let inputs: Vec<Input> = parse(input.to_string());
 
-        let _num = inputs.iter().for_each(|inp| println!("{:?}", inp.digits));
-
-        let num = inputs
-            .iter()
-            .flat_map(|inp| inp.digits.iter())
-            .filter(|&&n| match n {
-                2 => true, // 1
-                4 => true, // 4
-                3 => true, // 7
-                7 => true, // 8
-                _ => false,
-            })
-            .count();
-        dbg!(num);
         // .for_each(|inp| println!("{:?} | {:?}", inp.data, inp.digits));
     }
 }
