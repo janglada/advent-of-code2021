308531449133069c6ef69e5a6219439292cf7873
joan <janglada@gmail.com>
2021-12-31 15:37:56 +0100

    adjust gen.sh script

--- days/gen.sh
+++ days/gen.sh
@@ -1,1 +1,9 @@
-cargo generate --git https://github.com/janglada/cargo-generate-aoc-day.git --name day7
+#!/usr/bin/env bash
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+fi
+
+
+cargo generate --git https://github.com/janglada/cargo-generate-aoc-day.git --name day$1
