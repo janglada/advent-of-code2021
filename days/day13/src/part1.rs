6e7be1f184c03a6909b1e8bd88737f46ae2e5f0b
joan <janglada@gmail.com>
2022-01-15 16:40:17 +0100

    lint day 13

--- days/day13/src/part1.rs
+++ days/day13/src/part1.rs
@@ -7,12 +7,7 @@ pub async fn solve_part1() {
     let input: String = donwload_puzzle(13).await.unwrap();
     let grid = new_dot_grid::<895, 1311>(input);
     // println!("{}", grid.0);
-    let mut next_grid = grid.0;
-    grid.1.iter().for_each(|f| {
-        next_grid = next_grid.fold_at(f);
-    });
-
-    println!("{}", next_grid);
+    let mut next_grid = grid.0.fold_at(grid.1.iter().next().unwrap());
     println!("{}", next_grid.count_visible());
 }
 
--- days/day13/src/part2.rs
+++ days/day13/src/part2.rs
@@ -1,8 +1,17 @@
 extern crate core;
 
+use crate::shared::new_dot_grid;
 use core::utils::donwload_puzzle;
 use std::slice::Iter;
 
 pub async fn solve_part2() {
-    let input: String = donwload_puzzle(3).await.unwrap();
+    let input: String = donwload_puzzle(13).await.unwrap();
+    let grid = new_dot_grid::<895, 1311>(input);
+    // println!("{}", grid.0);
+    let mut next_grid = grid.0;
+    grid.1.iter().for_each(|f| {
+        next_grid = next_grid.fold_at(f);
+    });
+
+    println!("{}", next_grid);
 }
